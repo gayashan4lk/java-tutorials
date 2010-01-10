352a2a63f39487d3f946f797d87c0e4213a59110
Gayashan Wagachchi <gayashan4lk@gmail.com>
2010-01-10 01:01:01 -0400

    hello world

---
+++ lessons/Chan1.java
@@ -0,0 +1,20 @@
+/**
+ * @(#)Chan1.java
+ *
+ *
+ * @author 
+ * @version 1.00 2010/1/9
+ */
+
+
+public class Chan1 {
+
+    public Chan1() {
+    }
+    public static void main(String [] args){
+    	String chan="Chan ideals";
+    	int x= 20;
+    	System.out.printf("%s is good and  x is %d",chan,x);
+    }
+    
+}
---
+++ lessons/test1.java
@@ -0,0 +1,21 @@
+/**
+ * @(#)test1.java
+ *
+ *
+ * @author 
+ * @version 1.00 2010/1/9
+ */
+
+
+public class test1 {
+
+    public test1() {
+    }
+    
+     public static void main (String[] args) {
+     	
+     	
+     	System.out.println( " Happy new year ");
+}
+    
+}
