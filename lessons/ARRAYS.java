73549db6679b63ebad77fb786c67a1c930b5f533
Gayashan Wagachchi <gayashan4lk@gmail.com>
2011-04-14 23:47:01 -0400

    arrays

---
+++ lessons/ARRAYS.java
@@ -0,0 +1,15 @@
+class ARRAYS
+{
+      public static void main(String args[])
+	  {
+	       int x;
+		   Scanner G = new Scanner ;
+		   int x = G.nextint();
+		   
+		   System.out.printf(" this is my input : ", x );
+	  
+	  
+	  }
+
+
+}
