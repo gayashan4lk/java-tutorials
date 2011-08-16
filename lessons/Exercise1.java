fffbed70b27c68517c2a165212d215ae61981b61
Gayashan Wagachchi <gayashan4lk@gmail.com>
2011-08-16 11:20:01 -0400

    exercise 1

---
+++ lessons/Exercise1.java
@@ -0,0 +1,22 @@
+public class Exercise1 {
+
+static float computePay(float hours, float rate){
+                 float pay;
+				 pay = hours * rate;
+				 return pay;
+
+				 }
+				 
+		public static void main (String argv[]){
+		    float hours;
+			float payrate;
+			hours = 40f;
+			payrate = 12.5f;
+			
+			System.out.println("The pay is : " + ComputePay(hours.payrate));
+		     
+		
+		} 		 
+		
+
+}
